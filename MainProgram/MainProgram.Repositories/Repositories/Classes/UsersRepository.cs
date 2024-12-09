4707cce134ce0b72af60acc9e248a0b17f11f647
Dmitry1850 <afsfhdjkvgk@gmail.com>
2024-12-10 00:29:02 +0300

    Добавлена инъекция зависисмостей
    Инъекция зависисмостей для бд

---
+++ MainProgram/MainProgram.Repositories/Repositories/Classes/UsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using MainProgram.Model;
+
+namespace MainProgram.Repositories
+{
+    public class UsersRepository : IUserRepository
+    {
+        private DataBase dataBase = new DataBase();
+
+        public async Task AddUserAsync(User user)
+        {
+            await Task.Run(() => dataBase.Add(user));
+        }
+
+    }
+}
