69165e4c3a3db035b96e32ffa7a8a57ec167354f
Dmitry1850 <afsfhdjkvgk@gmail.com>
2024-12-09 18:22:20 +0300

    Добавлены модели

---
+++ MainProgram/WebApplication1/Models/Users/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MainProgram.Model
+{
+    public class User
+    {
+        public Guid userId { get; set; }
+        public string email { get; set; }
+        public string passwordHash { get; set; }
+        public string role { get; set; }
+        public string refreshToken { get; set; }
+        public DateTime refreshTokenExpiryTime { get; set; }
+    }
+}
