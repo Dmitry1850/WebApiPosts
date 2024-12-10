d5b8acb46b966aae394c06a0b38e887e70d3e410
Dmitry1850 <afsfhdjkvgk@gmail.com>
2024-12-10 18:53:45 +0300

    Сделана бд постов

---
+++ MainProgram/WebApplication1/Repositories/Classes/PostsRepository.cs
@@ -0,0 +1,34 @@
+﻿using MainProgram.Model;
+
+namespace MainProgram.Repositories
+{
+    public class PostsRepository : IPostRepository
+    {
+        private List<Post> posts = new List<Post>();
+
+        public async Task AddPost(Post post)
+        {
+            posts.Add(post);
+        }
+
+        public async Task DeletePost(Guid PostID)
+        {
+            for (int i = 0; i < posts.Count; i++)
+            {
+                if (PostID == posts[i].postId)
+                    await Task.Run(() => (posts.Remove(posts[i])));
+            }
+        }
+
+        public async Task<Post> ReturnPost(Guid PostID)
+        {
+            for (int i = 0; i < posts.Count; i++)
+            {
+                if (PostID == posts[i].postId)
+                    return await Task.Run(() => (posts[i]));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MainProgram/WebApplication1/Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using MainProgram.Model;
+
+namespace MainProgram.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<Post> ReturnPost(Guid PostID);
+        Task AddPost(Post post);
+        Task DeletePost(Guid PostID);
+    }
+}
