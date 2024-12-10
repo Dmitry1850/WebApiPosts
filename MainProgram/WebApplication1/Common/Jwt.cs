008c98f6a2d7e8e89e78da753d7badd591c77b66
Dmitry1850 <afsfhdjkvgk@gmail.com>
2024-12-10 19:06:07 +0300

    Написана часть токена

---
+++ MainProgram/WebApplication1/Common/Jwt.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MainProgram.Common
+{
+    public static class Jwt
+    {
+        public static string? GetId(string token)
+        {
+            return ParseToken(token, "UserID");
+        }
+    }
+}
