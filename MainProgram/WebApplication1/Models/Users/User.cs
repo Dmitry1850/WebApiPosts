f4b1b4ccd577ada12b78cebb2a1f75ecfac1937f
Dmitry1850 <afsfhdjkvgk@gmail.com>
2024-12-10 18:45:51 +0300

    пофикшены ошибки

---
+++ MainProgram/WebApplication1/Models/Users/User.cs
@@ -0,0 +1,21 @@
+﻿namespace MainProgram.Model
+{
+    public class User 
+    {
+        public User(string Email, string PasswordHash, string Role, string RefreshToken)
+        { 
+            userId = Guid.NewGuid();
+            email = Email;
+            passwordHash = PasswordHash;
+            role = Role;
+            refreshToken = RefreshToken;
+            refreshTokenExpiryTime = DateTime.Now;
+        }
+        public Guid userId { get; set; }
+        public string email { get; set; }
+        public string passwordHash { get; set; }
+        public string role { get; set; } // author or reader Все в НИЖНЕМ регистре!!!!!!!!!!
+        public string refreshToken { get; set; }
+        public DateTime refreshTokenExpiryTime { get; set; }
+    }
+}
---
+++ MainProgram/WebApplication1/Repositories/Classes/UsersRepository.cs
@@ -0,0 +1,24 @@
+﻿using MainProgram.Model;
+
+namespace MainProgram.Repositories
+{
+    public class UsersRepository : IUserRepository
+    {
+        private List<User> usersBase = new List<User>();
+
+        public async Task<User> ReturnUser(string Email)
+        {
+            for (int i = 0; i < usersBase.Count; i++)
+            {
+                if (usersBase[i].email == Email)
+                    return await Task.Run(() => (usersBase[i]));
+            }
+
+            return null;
+        }
+        public async Task AddUser(User user)
+        {
+            await Task.Run(() => usersBase.Add(user));
+        }
+    }
+}
